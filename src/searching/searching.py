a60f1dfedbbb099b183558a97928b11315d3cf76
Blayze <blayzestone@protonmail.com>
2020-07-20 18:28:10 -0700

    solve searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
 def linear_search(arr, target):
-    # Your code here
+    for idx, val in enumerate(arr):
+        if val == target:
+            return idx
 
-
-    return -1   # not found
+    return -1  # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    left_half = 0
+    right_half = len(arr) - 1
 
-    # Your code here
+    while left_half <= right_half:
+        mid = (right_half + left_half) // 2
 
+        if target == arr[mid]:
+            return mid
+        elif target < arr[mid]:
+            right_half = mid - 1
+        else:
+            left_half = mid + 1
 
     return -1  # not found
